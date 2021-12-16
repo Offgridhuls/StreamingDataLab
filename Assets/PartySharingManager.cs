b5b70ad9645d07b90d136c8432bdeca58c74e7c7
Offgridhuls <55862297+Offgridhuls@users.noreply.github.com>
2021-12-16 16:13:12 -0500

    Added sharing rooms
    - Can create sharing rooms
    - Can join sharing rooms

---
+++ Assets/PartySharingManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PartySharingManager : MonoBehaviour
+{
+
+    public GameObject enterSharingRoomButton, sharingRoomNameText,
+        sharingRoomInputField, sendPartyButton;
+
+    public NetworkedClient networkedClient;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enterSharingRoomButton.GetComponent<Button>().onClick.AddListener(JoinSharingRoomButtonPressed);
+        sendPartyButton.GetComponent<Button>().onClick.AddListener(SendPartyButtonPressed);
+
+        networkedClient = GetComponent<NetworkedClient>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SendPartyButtonPressed()
+    {
+
+
+    }
+    private void JoinSharingRoomButtonPressed()
+    {
+
+        string name = sharingRoomInputField.GetComponent<InputField>().text;
+        networkedClient.SendMessageToHost(ClientToServerSignifiers.JoinSharingRoom + "," + name);
+    }
+}
